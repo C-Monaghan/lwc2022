e15960cd533a4d6a1d1b78805b605dcbf68c184f
C-Monaghan <cormacmonaghan@tutanota.com>
2024-11-14 14:17:59 +0000

    Updating page

--- data-raw/load.R
+++ data-raw/load.R
@@ -14,6 +14,11 @@ generate_example_data <- function(n = 100) {
     SD182M3 = sample(c(1:40, 51:67, 96, 98, 99), n, replace = TRUE),
     SD182M4 = sample(c(1:40, 51:67, 96, 98, 99), n, replace = TRUE),
     SD182M5 = sample(c(1:40, 51:67, 96, 98, 99), n, replace = TRUE),
+    SD182M6 = sample(c(1:40, 51:67, 96, 98, 99), n, replace = TRUE),
+    SD182M7 = sample(c(1:40, 51:67, 96, 98, 99), n, replace = TRUE),
+    SD182M8 = sample(c(1:40, 51:67, 96, 98, 99), n, replace = TRUE),
+    SD182M9 = sample(c(1:40, 51:67, 96, 98, 99), n, replace = TRUE),
+    SD182M10 = sample(c(1:40, 51:67, 96, 98, 99), n, replace = TRUE),
 
     # Delayed word recall (5 items)
     SD183M1 = sample(c(1:40, 51:67, 96, 98, 99), n, replace = TRUE),
@@ -21,6 +26,11 @@ generate_example_data <- function(n = 100) {
     SD183M3 = sample(c(1:40, 51:67, 96, 98, 99), n, replace = TRUE),
     SD183M4 = sample(c(1:40, 51:67, 96, 98, 99), n, replace = TRUE),
     SD183M5 = sample(c(1:40, 51:67, 96, 98, 99), n, replace = TRUE),
+    SD183M6 = sample(c(1:40, 51:67, 96, 98, 99), n, replace = TRUE),
+    SD183M7 = sample(c(1:40, 51:67, 96, 98, 99), n, replace = TRUE),
+    SD183M8 = sample(c(1:40, 51:67, 96, 98, 99), n, replace = TRUE),
+    SD183M9 = sample(c(1:40, 51:67, 96, 98, 99), n, replace = TRUE),
+    SD183M10 = sample(c(1:40, 51:67, 96, 98, 99), n, replace = TRUE),
 
     # Serial subtraction (Subtracting 7 from 100 five times)
     SD142 = sample(90:100, n, replace = TRUE),  # First subtraction value
@@ -35,11 +45,11 @@ generate_example_data <- function(n = 100) {
   )
 }
 
-# Generate a dataset of 100 observations
+# Generate a dataset of 10 observations
 cog_data <- generate_example_data(n = 10)
 
 # Scoring dataset
-cog_data_score <- LWC2022::score(cog_data)
+cog_data_score <- lwc2022::score(cog_data)
 
 # Use these as example dataset
 usethis::use_data(cog_data, overwrite = TRUE)
